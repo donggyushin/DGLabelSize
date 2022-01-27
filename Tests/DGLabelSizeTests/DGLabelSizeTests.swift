005f83a7458b3e024042ec0dcee9ade7859ae0fe
신동규 <donggyu9410@gmail.com>
2022-01-27 13:48:08 +0900

    remove tests

--- Tests/DGLabelSizeTests/DGLabelSizeTests.swift
+++ Tests/DGLabelSizeTests/DGLabelSizeTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import DGLabelSize
 
 final class DGLabelSizeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DGLabelSize().text, "Hello, World!")
-    }
+    
 }
