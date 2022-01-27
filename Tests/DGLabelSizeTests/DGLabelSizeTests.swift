23e50b9dd33ff71838c88e2fa1d693a2abe8a1fb
신동규 <donggyu9410@gmail.com>
2022-01-27 14:11:01 +0900

    Add test case

--- Tests/DGLabelSizeTests/DGLabelSizeTests.swift
+++ Tests/DGLabelSizeTests/DGLabelSizeTests.swift
@@ -3,4 +3,31 @@ import XCTest
 
 final class DGLabelSizeTests: XCTestCase {
     
+    private let label1: UILabel = {
+        let view = UILabel()
+        view.text = "short text"
+        return view
+    }()
+    
+    private let label2: UILabel = {
+        let view = UILabel()
+        view.text = "long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text long text "
+        view.numberOfLines = 0
+        return view
+    }()
+    
+    private let maxWidth: CGFloat = 350.0
+    private let maxHeight: CGFloat = 1000.0
+    
+    func testDGLabelSizeHeight() {
+        XCTAssertEqual(DGLabelSize.height(maxWidth: maxWidth, maxHeight: maxHeight, label1), 21)
+        XCTAssertEqual(DGLabelSize.height(maxWidth: maxWidth, maxHeight: maxHeight, label2), 102)
+    }
+    
+    func testDGLabelSizeWidth() {
+        XCTAssertEqual(DGLabelSize.width(maxHeight: maxHeight, maxWidth: maxWidth, label1), 73)
+        XCTAssertEqual(DGLabelSize.width(maxHeight: maxHeight, maxWidth: maxWidth, label2), 350)
+        
+    }
+    
 }
