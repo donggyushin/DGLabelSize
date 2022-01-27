254e04c36d370620afa8ff974ae87465f2f2e9f4
신동규 <donggyu9410@gmail.com>
2022-01-27 14:07:11 +0900

    fix error

--- Sources/DGLabelSize/DGLabelSize.swift
+++ Sources/DGLabelSize/DGLabelSize.swift
@@ -1,15 +1,18 @@
 import UIKit
 
-public func height(withConstrainedWidth width: CGFloat, _ label: UILabel) -> CGFloat {
+public func height(maxWidth width: CGFloat, maxHeight height: CGFloat, _ label: UILabel) -> CGFloat {
     guard let string = label.text else { return 0 }
     let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
     let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font ?? .init()], context: nil)
-    return ceil(boundingBox.height)
+    
+    let result = ceil(boundingBox.height)
+    return result > height ? height : result
 }
 
-public func width(withConstrainedHeight height: CGFloat, _ label: UILabel) -> CGFloat {
+public func width(maxHeight height: CGFloat, maxWidth width: CGFloat, _ label: UILabel) -> CGFloat {
     guard let string = label.text else { return 0 }
     let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
     let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font ?? .init()], context: nil)
-    return ceil(boundingBox.width)
+    let result = ceil(boundingBox.width)
+    return result > width ? width : result
 }
