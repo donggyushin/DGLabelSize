40f055453eaa3f6903c4a082dcfcb61da6937add
신동규 <donggyu9410@gmail.com>
2022-01-27 13:49:08 +0900

    Add functions

--- Sources/DGLabelSize/DGLabelSize.swift
+++ Sources/DGLabelSize/DGLabelSize.swift
@@ -1,6 +1,15 @@
-public struct DGLabelSize {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
-    }
+public func height(withConstrainedWidth width: CGFloat, _ label: UILabel) -> CGFloat {
+    guard let string = label.text else { return 0 }
+    let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+    let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font ?? .init()], context: nil)
+    return ceil(boundingBox.height)
+}
+
+public func width(withConstrainedHeight height: CGFloat, _ label: UILabel) -> CGFloat {
+    guard let string = label.text else { return 0 }
+    let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+    let boundingBox = string.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font ?? .init()], context: nil)
+    return ceil(boundingBox.width)
 }
